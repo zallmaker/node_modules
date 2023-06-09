c8be79e457f9ee0384fda16ed3955bdf722db34c
zallmaker <scytzall01@gmail.com>
2023-06-09 13:35:28 +0700

    Gosen

---
+++ api-dylux/main.js
@@ -0,0 +1,25 @@
+const ds = require('./index.js')
+ 
+module.exports.xvideosSearch = ds.xvideosSe
+module.exports.xnxxSearch = ds.xnxxSe
+module.exports.xvideosdl = ds.xvideosdl
+module.exports.xnxxdl = ds.xnxxdl
+module.exports.tiktok = ds.tiktokE
+module.exports.GDriveDl = ds.Gdrive
+module.exports.fbdl = ds.facebookE
+module.exports.twitter = ds.twitter
+module.exports.pinterest = ds.Pinterest
+module.exports.StickerSearch = ds.StickerSearch
+module.exports.Emoji = ds.Emoji
+module.exports.mediafireDl = ds.mediafireDlE
+module.exports.igStalk = ds.igStalk
+module.exports.ttStalk = ds.tiktokStalk
+module.exports.sfileDl = ds.sfileDl
+module.exports.sfileSearch = ds.sfileSe
+module.exports.wallpaper = ds.wphd
+module.exports.igdl = ds.igdl
+module.exports.ttp = ds.ttp
+module.exports.ssweb = ds.sswebE
+module.exports.ytdl = ds.youtube
+module.exports.lyrics = ds.Lyrics
+module.exports.googleImage = ds.gimg
